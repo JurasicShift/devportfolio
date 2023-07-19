e5395b49bef1808fea88f225d640825b474ccc47
JurasicShift <64592484+JurasicShift@users.noreply.github.com>
2023-07-19 16:33:19 +0100

    moved server file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const path = require('path');
 const app = express();
 const helmet = require("helmet");
-const routes = require('./routes/index');
+const routes = require('./backend/routes/index');
 
 const environment = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 5000;
@@ -29,9 +29,9 @@ app.use('/', routes);
 //Serve Frontend
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, './frontend/build')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './', 'frontend', 'build', 'index.html')));
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
 }
