fbb737aebd4598575045f59ab16b81d5bbfeae1e
JurasicShift <64592484+JurasicShift@users.noreply.github.com>
2023-08-16 14:51:22 +0100

    updated statement

--- frontend/src/components/About/AboutDraft.jsx
+++ frontend/src/components/About/AboutDraft.jsx
@@ -41,21 +41,15 @@ const Draft = () => {
 	return (
 		<>
 			<p className="aboutme__text has-dropcap">
-				Michael Whyte is a self taught M.E.R.N. stack developer based in
-				Lancaster, England. Michael works as a library assistant and is
-				currently seeking employment opportunities in web development.{' '}
+				Hello and welcome to my portfolio. I am a self-taught M.E.R.N. stack developer based in Lancaster, England. I work as a library assistant and I'm currently seeking employment opportunities in web development.{' '}
 			</p>
 			<p className="aboutme__text">
 				{' '}
-				Driven by a love of learning, his interest in web development is a
-				natural concomitant and extension of his work with the Library Service.
-				Coming from an Arts and Humanities background (MA Critical Theory, BA(Hons) Film
-				& Media Studies), and having little previous knowledge of STEM subjects, for Michael web
-				development emerged straight out of leftfield. Here is the story of how this came to fruition.
+				Driven by a love of learning, my interest in web development is a natural concomitant and extension of my work with the Library Service. Coming from an Arts and Humanities background (MA Critical Theory, BA(Hons) Film and Media Studies), and having little previous knowledge of STEM subjects, for me, web development emerged straight out of leftfield. Here is the story of how this came to fruition.
 			</p>
 			<p className="aboutme__text">
 				Part of the mission statement of public libraries is to promote digital
-				inclusion. Since 2018 Michael has delivered digital support to students
+				inclusion. Since 2018 I have delivered digital support to students
 				on the{' '}
 				<span>
 					<a
@@ -70,7 +64,7 @@ const Draft = () => {
 					</a>
 				</span>{' '}
 				course. To deliver the best possible educative experience and enhance
-				his own digital skills, in 2019 Michael completed the Essential Digital
+				my own digital skills, in 2019 I completed the Essential Digital
 				Skills Level Two with the
 				<span>
 					<a
@@ -91,14 +85,7 @@ const Draft = () => {
 				. The seeds were sown for a growing interest in the active use of computers.
 			</p>
 			<p className="aboutme__text">
-				A turning point came in early 2020 when Michael was asked to run a code
-				club for kids. Having no experience with code he was hesitant but
-				figured, if kids can code then I can too. With this in mind he borrowed
-				a kids book on HTML and CSS and began experimenting. Struck by
-				the power of HTML and CSS to produce online content, their syntactical composition and
-				structural interrelation and the exchange
-				between technology and creativity, he was hooked. The kids book became an adult
-				book which became a web development bootcamp on{' '}
+			A turning point came in early 2020 when I was asked to run a code club for kids. Having no experience with code I was hesitant but figured, if kids can code then I can too. With this in mind I borrowed a kids book on HTML and CSS and began experimenting. Struck by the power of HTML and CSS to produce online content, their syntactical composition and structural interrelation and the exchange between technology and creativity, I was hooked. The kids book became an adult book which became a web development bootcamp on{' '}
 				<span>
 					<a
 						href="https://www.udemy.com/course/the-web-developer-bootcamp/"
@@ -110,7 +97,7 @@ const Draft = () => {
 						<img src={Udemy} style={udemyStyle} alt="udemy" />
 					</a>
 				</span>
-				. Since September 2022 Michael has delivered a weekly{' '}
+				. Since September 2022 I have delivered a weekly{' '}
 				<span>
 					<a
 						href="https://coderdojo.com/en"
@@ -126,14 +113,7 @@ const Draft = () => {
 				for kids aged 7 – 11.
 			</p>
 			<p className="aboutme__text aboutme__text--mod">
-				Beyond web development his interests are split between outdoor
-				activities and more academic pursuits. Michael has completed 5 marathons
-				and 6 half marathons up and down the UK. He enjoys travel and gained his advanced open water diving certificate in Indonesia. He
-				recently walked the Cumbria Way and is currently training to swim the
-				length of Coniston Water (5.25 miles). Michael enjoys learning French
-				and reading French literature. His other reading interests include:
-				history, politics, philosophy, critical theory, psychoanalysis, science
-				fiction and classics.
+			Beyond web development my interests are split between outdoor activities and more academic pursuits. I have completed 5 marathons and 6 half marathons up and down the UK. I enjoy travel and have gained my advanced open water diving certificate in Indonesia. I recently walked the Cumbria Way and I'm currently training to swim the length of Coniston Water (5.25 miles). I enjoy learning French and reading French literature. My other reading interests include history, politics, philosophy, critical theory, psychoanalysis, science fiction and classics
 			</p>
 		</>
 	);
